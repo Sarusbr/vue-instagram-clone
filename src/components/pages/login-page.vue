5315c90c0bb2bd8e539ee473804afe2cf8be2c7e
Sarusbr <87176573+Sarusbr@users.noreply.github.com>
2022-09-20 14:11:23 +0300

    login page

---
+++ src/components/pages/login-page.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container center">
+        <login-box></login-box>
+    </div>
+</template>
+
+<script>
+import loginBox from "../tools/loginBox.vue";
+export default {
+    components:{
+        loginBox
+    }
+}
+</script>
+
+<style>
+.center{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
